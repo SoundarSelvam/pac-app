08adab02af7378abf9b8ee5aa29ce36035400f32
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-06 22:03:27 +0530

    update by murugan

--- src/main/java/pac/app/controller/EcfGetController.java
+++ src/main/java/pac/app/controller/EcfGetController.java
@@ -10,23 +10,49 @@ import org.apache.commons.logging.LogFactory;
 public class EcfGetController {
     private static final Log LOG = LogFactory.getLog(EcfGetController.class);
 
-    @Post(value = "/book", consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_FORM_URLENCODED})
-    @Status(HttpStatus.CREATED)
-    Book search(@Body Book book) {
-        LOG.info(book.toString());
-        return book;
-    }
-
-    @Post("/pick/{rank}")
-    public String kick(String rank) {
-        LOG.info(rank.toString());
-        return rank;
-    }
-
-    @Post("/pi/{rank}/{jan}")
-    public String pi(String rank,String jan) {
-        LOG.info(rank.toString());
-        return rank+jan;
+    @Get("/pe001")
+    public String getEvent(@Body String body) {
+        LOG.info("Local Test7");
+        body = "jan:1234567ABCDEF";
+        LOG.info(body);
+        String [] s1 = body.split(":");
+        String jan = s1[1];
+        LOG.info(jan + "::" + s1[1].length());
+        amazonDynamoDBClient = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .withRegion(Regions.US_EAST_1).build();
+        HashMap<String, Condition> scanFilter = new HashMap<>();
+        Condition condition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
+                .withAttributeValueList(new AttributeValue().withS(jan));
+        scanFilter.put("jan", condition);
+        ScanRequest scanRequest1 = new ScanRequest("pac_all").withScanFilter(scanFilter);
+        ScanResult scanResult1 = amazonDynamoDBClient.scan(scanRequest1);
+        List<java.util.Map<String, AttributeValue>> aa = scanResult1.getItems();
+        LOG.info(aa.size());
+        AttributeValue cc = new AttributeValue();
+        String base_masterStoreCode = "";
+        String base_maStoreCode = "";
+        String base_promotionCode = "";
+        String base_rewardCode = "";
+        String base_promotionDesc = "";
+        String base_point = "";
+        for (int i = 1; i < aa.size(); i++) {
+            java.util.Map<String, AttributeValue> bb = aa.get(i);
+            Iterator<String> iterator = bb.keySet().iterator();
+            while (iterator.hasNext()) {
+                String key = iterator.next();
+                cc = bb.get(key);
+                if (key.equals("jan")) {
+                    base_masterStoreCode = jan.substring(0, 4);
+                    base_maStoreCode = jan.substring(4, 6);
+                    base_promotionCode = jan.substring(6, 10);
+                    base_rewardCode = jan.substring(10);
+                }
+            }
+            LOG.info(cc.toString());
+            LOG.info(base_masterStoreCode);
+        }
+        return "{\"MasterStroreCode\":\"" + base_masterStoreCode + "\",\"MaStoreCode\":\"" + base_maStoreCode + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"RewardCode\":\"" + base_rewardCode + "\"}";
     }
 
 }
