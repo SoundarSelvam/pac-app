8c1f8f838cc2dd8852317e903a10adf84f30b321
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-09 00:01:00 +0530

    update by murugan

--- src/main/java/pac/app/controller/EcfGetController.java
+++ src/main/java/pac/app/controller/EcfGetController.java
@@ -45,6 +45,7 @@ public class EcfGetController {
     private static final Logger LOG = LoggerFactory.getLogger(EcfGetController.class);
     private static AmazonDynamoDB amazonDynamoDBClient = null;
     private static Table table = null;
+
     @Get("/pe001")
     @Produces(MediaType.APPLICATION_JSON)
     public String getEvent(@Body String body) {
@@ -54,9 +55,9 @@ public class EcfGetController {
         LOG.info("Local Test7 murugan");
         body = "jan:1234567890234";
         LOG.info(body);
-        String [] s1 = body.split(":");
+        String[] s1 = body.split(":");
         String jan = s1[1];
-        String rank="2";
+        String rank = "2";
         LOG.info(jan + "::" + s1[1].length());
         HashMap<String, Condition> scanFilter = new HashMap<>();
         Condition condition = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
@@ -64,7 +65,7 @@ public class EcfGetController {
         Condition condition1 = new Condition().withComparisonOperator(ComparisonOperator.EQ.toString())
                 .withAttributeValueList(new AttributeValue().withS(rank));
         scanFilter.put("jan", condition);
-        scanFilter.put("rank",condition1);
+        scanFilter.put("rank", condition1);
         ScanRequest scanRequest1 = new ScanRequest("pac_all").withScanFilter(scanFilter);
         ScanResult scanResult1 = amazonDynamoDBClient.scan(scanRequest1);
         List<java.util.Map<String, AttributeValue>> aa = scanResult1.getItems();
@@ -82,13 +83,12 @@ public class EcfGetController {
                 String key = iterator.next();
                 cc = bb.get(key);
                 if (key.equals("jan")) {
-                    base_masterStoreCode = jan.substring(0,5);
-                    base_maStoreCode = jan.substring(5,6);
-                    base_promotionCode = jan.substring(6,10);
+                    base_masterStoreCode = jan.substring(0, 5);
+                    base_maStoreCode = jan.substring(5, 6);
+                    base_promotionCode = jan.substring(6, 10);
                     base_rewardCode = jan.substring(10);
                 }
-                if(key.equals("PromotionDesc"))
-                {
+                if (key.equals("PromotionDesc")) {
                     base_promotionDesc = cc.toString().substring(4);
                     base_promotionDesc = base_promotionDesc.substring(0, base_promotionDesc.length() - 2);
                 }
@@ -96,24 +96,24 @@ public class EcfGetController {
                     base_point = cc.toString().substring(4);
                     base_point = base_point.substring(0, base_point.length() - 2);
                 }
-            LOG.info(cc.toString());
-            LOG.info(base_masterStoreCode);
+                LOG.info(cc.toString());
+                LOG.info(base_masterStoreCode);
+            }
+            // return "{\"jan\":\"" + jan + "\",\"MasterStroreCode\":\"" + base_masterStoreCode + "\",\"MaStoreCode\":\"" + base_maStoreCode + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"RewardCode\":\"" + base_rewardCode + "\"}";
+            return "{\n" +
+                    "  \"MEMBER_INFO\": {\n" +
+                    "    \"MEMBER_RANK\": \"" + rank + "\",\n" +
+                    "    \"PROMOTION\": [\n" +
+                    "      {\n" +
+                    "        \"PROMOTION_CODE\":\"" + base_promotionCode + "\",\n" +
+                    "        \"PROMOTION_DESC\": \"" + base_promotionDesc + "\",\n" +
+                    "        \"ITERM-INFO\": [\n" +
+                    "          {\"JAN_CODE\":\"" + jan + "\", \"POINT_PLUS\":\"" + base_point + "\",\"STORE_CODE\":\"" + base_maStoreCode + "\"},\n" +
+                    "        ]\n" +
+                    "      },\n" +
+                    "    ]\n" +
+                    "  }\n" +
+                    "\"}";
         }
-       // return "{\"jan\":\"" + jan + "\",\"MasterStroreCode\":\"" + base_masterStoreCode + "\",\"MaStoreCode\":\"" + base_maStoreCode + "\",\"PromotionCode\":\"" + base_promotionCode + "\",\"RewardCode\":\"" + base_rewardCode + "\"}";
-        return  "{\n" +
-                "  \"MEMBER_INFO\": {\n" +
-                "    \"MEMBER_RANK\": \""+ rank+"\",\n" +
-                "    \"PROMOTION\": [\n" +
-                "      {\n" +
-                "        \"PROMOTION_CODE\":\""+ base_promotionCode+"\",\n" +
-                "        \"PROMOTION_DESC\": \""+base_promotionDesc+"\",\n" +
-                "        \"ITERM-INFO\": [\n" +
-                "          {\"JAN_CODE\":\"" +jan+"\", \"POINT_PLUS\":\""+ base_point+"\",\"STORE_CODE\":\""+ base_maStoreCode+"\"},\n" +
-                "        ]\n" +
-                "      },\n" +
-                "    ]\n" +
-                "  }\n" +
-                "}";
     }
-}
 }
