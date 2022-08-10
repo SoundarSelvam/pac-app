260b410002d8b67871a680c32d5556376b848758
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-10 10:40:41 +0530

    update by murugan

--- src/main/java/pac/app/controller/EcfPostController.java
+++ src/main/java/pac/app/controller/EcfPostController.java
@@ -119,10 +119,10 @@ public class EcfPostController {
 //        connection.setDoOutput(true);
         JSONObject inputObject = new JSONObject(body);
         //JSONArray subjects = (JSONArray)jsonObject.get("jan");
-        String janCode = inputObject.get("jan");
-        String rank = inputObject.get("rank");
-        String point = inputObject.get("point");
-        String storeCode =inputObject.get("storeCode");
+        String janCode = (String)inputObject.get("jan");
+        String rank = (String)inputObject.get("rank");
+        String point = (String)inputObject.get("point");
+        String storeCode =(String)inputObject.get("storeCode");
         String[] s1 = janCode.split(":");
         String jan = s1[1];
         HashMap<String, Condition> scanFilter = new HashMap<>();
