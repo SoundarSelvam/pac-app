407f09ecb31cfe482c024c3efd2a8e9bceeb9a26
Murugan Ramalingam <murugan.r-external@tcs.com>
2022-08-16 11:08:55 +0530

    update by murugan

--- src/main/java/pac/app/controller/EcfPostController.java
+++ src/main/java/pac/app/controller/EcfPostController.java
@@ -109,14 +109,15 @@ public class EcfPostController {
                 .withRegion(Regions.AP_NORTHEAST_1).build();
         LOG.info(body);
         String[] j = body.split(",");
-        String jan =j[1].substring(8,j[1].length()-1);
-        String rank =j[2].substring(8,j[2].length()-1);
+        String jan = j[1].substring(8,j[1].length()-1);
+        //String rank =j[2].substring(8,j[2].length()-1);
         //String point =j[3].substring(9,j[3].length()-1);
         LOG.info(jan);
+        //LOG.info(rank);
 //        String rank = getBody.getRank();
 //        String point = getBody.getPoint();
 //        String jan = "1234567890234";
-         //String rank = "2";
+        String rank = "2";
 //        //String point = "800";
         LOG.info("Local_Test4_murugan");
         HashMap<String, Condition> scanFilter = new HashMap<>();
